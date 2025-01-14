8c3a2f08ee2db8e1ee69aed48d289acc2d373621
Santi <canosantiago404@gmail.com>
2025-01-14 12:21:14 -0300

    feat: added ui components

---
+++ components/Divider.tsx
@@ -0,0 +1,19 @@
+import { View } from "react-native";
+
+import { ShortcutProps, defaultShortcuts } from "@/styles/shortcuts";
+
+export interface DividerProps extends ShortcutProps {}
+
+export const Divider = (props: DividerProps) => {
+  return (
+    <View
+      style={[
+        defaultShortcuts(props),
+        {
+          backgroundColor: "lightgray",
+          height: 1,
+        },
+      ]}
+    />
+  );
+};
---
+++ components/Text.tsx
@@ -0,0 +1,40 @@
+import { PropsWithChildren } from "react";
+import { Text as RNText, TextProps } from "react-native";
+
+import { defaultShortcuts, ShortcutProps } from "@/styles/shortcuts";
+
+export interface CustomTextProps
+  extends PropsWithChildren,
+    ShortcutProps,
+    TextProps {
+  fontSize?: number;
+  bold?: boolean;
+  underline?: boolean;
+  color?: string;
+}
+
+export const Text = ({
+  fontSize = 18,
+  bold,
+  underline,
+  color,
+  children,
+  ...restProps
+}: CustomTextProps) => {
+  return (
+    <RNText
+      style={[
+        defaultShortcuts(restProps),
+        {
+          fontSize,
+          fontWeight: bold ? "bold" : "normal",
+          textDecorationLine: underline ? "underline" : "none",
+          color,
+        },
+      ]}
+      {...restProps}
+    >
+      {children}
+    </RNText>
+  );
+};
