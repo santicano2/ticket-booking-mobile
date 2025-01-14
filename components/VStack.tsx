0aa5498aefd4427d89c02a7c5f973c194b0b7fdc
Santi <canosantiago404@gmail.com>
2025-01-14 10:56:59 -0300

    feat: added Stacks components

---
+++ components/VStack.tsx
@@ -0,0 +1,11 @@
+import { Stack, StackProps } from "@/components/Stack";
+
+interface VStackProps extends StackProps {}
+
+export function VStack(props: VStackProps) {
+  return (
+    <Stack {...props} direction="column">
+      {props.children}
+    </Stack>
+  );
+}
