e3d28b5cc1335849296ad2eacceff3edf8b5fb47
Santi <canosantiago404@gmail.com>
2025-01-15 10:28:17 -0300

    feat: added event type and service

---
+++ services/event.ts
@@ -0,0 +1,41 @@
+import { Api } from "./api";
+import { EventListResponse, EventResponse } from "@/types/event";
+
+async function createOne(
+  name: string,
+  location: string,
+  date: string
+): Promise<EventResponse> {
+  return Api.post("/event", { name, location, date });
+}
+
+async function getOne(id: number): Promise<EventResponse> {
+  return Api.get(`/event/${id}`);
+}
+
+async function getAll(): Promise<EventListResponse> {
+  return Api.get("/event");
+}
+
+async function updateOne(
+  id: number,
+  name: string,
+  location: string,
+  date: string
+): Promise<EventResponse> {
+  return Api.put(`/event/${id}`, { name, location, date });
+}
+
+async function deleteOne(id: number): Promise<EventResponse> {
+  return Api.delete(`/event/${id}`);
+}
+
+const eventService = {
+  createOne,
+  getOne,
+  getAll,
+  updateOne,
+  deleteOne,
+};
+
+export { eventService };
