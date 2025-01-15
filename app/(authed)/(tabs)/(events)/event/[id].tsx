f60483c763ba64c1ac7ee4116cc72a0398d84df9
Santi <canosantiago404@gmail.com>
2025-01-15 12:40:44 -0300

    feat: added event detail screen

--- app/(authed)/(tabs)/(events)/event/[id].tsx
+++ app/(authed)/(tabs)/(events)/event/[id].tsx
@@ -1,5 +1,145 @@
-import React from "react";
+import { useCallback, useEffect, useState } from "react";
+import {
+  router,
+  useFocusEffect,
+  useLocalSearchParams,
+  useNavigation,
+} from "expo-router";
+import { Alert } from "react-native";
+
+import { Event } from "@/types/event";
+import { eventService } from "@/services/event";
+
+import { VStack } from "@/components/VStack";
+import { Text } from "@/components/Text";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 export default function EventDetailScreen() {
-  return <></>;
+  const navigation = useNavigation();
+  const { id } = useLocalSearchParams();
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [eventData, setEventData] = useState<Event | null>(null);
+
+  function updateField(field: keyof Event, value: string | Date) {
+    setEventData((prev) => ({
+      ...prev!,
+      [field]: value,
+    }));
+  }
+
+  const onDelete = useCallback(async () => {
+    if (!eventData) return;
+    try {
+      Alert.alert(
+        "Borrar evento",
+        "Estas seguro que quieres borrar este evento?",
+        [
+          { text: "Cancelar" },
+          {
+            text: "Borrar",
+            onPress: async () => {
+              await eventService.deleteOne(Number(id));
+              router.back();
+            },
+          },
+        ]
+      );
+    } catch (error) {
+      Alert.alert("Error", "Error al borrar el evento");
+    }
+  }, [eventData, id]);
+
+  async function onSubmitChanges() {
+    if (!eventData) return;
+    try {
+      setIsSubmitting(true);
+      await eventService.updateOne(
+        Number(id),
+        eventData.name,
+        eventData.location,
+        eventData.date
+      );
+      router.back();
+    } catch (error) {
+      Alert.alert("Error", "Error al actualizar el evento");
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
+  const fetchEvent = async () => {
+    try {
+      const response = await eventService.getOne(Number(id));
+      setEventData(response.data);
+    } catch (error) {
+      router.back();
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchEvent();
+    }, [])
+  );
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: "",
+      headerRight: () => headerRight(onDelete),
+    });
+  }, [navigation, onDelete]);
+
+  return (
+    <VStack m={20} flex={1} gap={30}>
+      <VStack gap={5}>
+        <Text ml={10} fontSize={14} color="gray">
+          Nombre
+        </Text>
+        <Input
+          value={eventData?.name}
+          onChangeText={(value) => updateField("name", value)}
+          placeholder="Name"
+          placeholderTextColor="darkgray"
+          h={48}
+          p={14}
+        />
+      </VStack>
+
+      <VStack gap={5}>
+        <Text ml={10} fontSize={14} color="gray">
+          Ubicación
+        </Text>
+        <Input
+          value={eventData?.location}
+          onChangeText={(value) => updateField("location", value)}
+          placeholder="Name"
+          placeholderTextColor="darkgray"
+          h={48}
+          p={14}
+        />
+      </VStack>
+
+      <VStack gap={5}>
+        <Text ml={10} fontSize={14} color="gray">
+          Fecha
+        </Text>
+      </VStack>
+
+      <Button
+        mt={"auto"}
+        isLoading={isSubmitting}
+        disabled={isSubmitting}
+        onPress={onSubmitChanges}
+      >
+        Guardar cambios
+      </Button>
+    </VStack>
+  );
 }
+
+const headerRight = (onPress: VoidFunction) => {
+  return <TabBarIcon size={30} name="trash" onPress={onPress} />;
+};
