9c2a457cedf504d67a230fe9d6bc68463876cf68
Santi <canosantiago404@gmail.com>
2025-01-16 11:21:27 -0300

    feat: scan tickets with expo-camera

--- app/(authed)/(tabs)/scan-ticket/_layout.tsx
+++ app/(authed)/(tabs)/scan-ticket/_layout.tsx
@@ -1,5 +1,68 @@
-import React from "react";
+import { useState } from "react";
+import {
+  BarcodeScanningResult,
+  CameraView,
+  useCameraPermissions,
+} from "expo-camera";
+import { ActivityIndicator, Alert, Vibration } from "react-native";
+
+import { ticketService } from "@/services/ticket";
+
+import { VStack } from "@/components/VStack";
+import { Text } from "@/components/Text";
+import { Button } from "@/components/Button";
 
 export default function ScanTicketScreen() {
-  return <></>;
+  const [permission, requestPermission] = useCameraPermissions();
+  const [scanningEnabled, setScanningEnabled] = useState(true);
+
+  if (!permission) {
+    return (
+      <VStack flex={1} justifyContent="center" alignItems="center">
+        <ActivityIndicator size={"large"} />
+      </VStack>
+    );
+  }
+
+  if (!permission.granted) {
+    return (
+      <VStack gap={20} flex={1} justifyContent="center" alignItems="center">
+        <Text>El permiso de la cámara es necesario para escanear tickets</Text>
+        <Button onPress={requestPermission}>Solicitar permiso</Button>
+      </VStack>
+    );
+  }
+
+  async function onBarcodeScanned({ data }: BarcodeScanningResult) {
+    if (!scanningEnabled) return;
+
+    try {
+      Vibration.vibrate();
+      setScanningEnabled(false);
+
+      const [ticket, owner] = data.split(",");
+      const ticketId = parseInt(ticket.split(":")[1]);
+      const ownerId = parseInt(owner.split(":")[1]);
+
+      await ticketService.validateOne(ticketId, ownerId);
+
+      Alert.alert("Success", "Ticket validado exitosamente", [
+        { text: "Ok", onPress: () => setScanningEnabled(true) },
+      ]);
+    } catch (error) {
+      Alert.alert("Error", "Error al validar el ticket. Inténtalo de nuevo.");
+      setScanningEnabled(true);
+    }
+  }
+
+  return (
+    <CameraView
+      style={{ flex: 1 }}
+      facing="back"
+      onBarcodeScanned={onBarcodeScanned}
+      barcodeScannerSettings={{
+        barcodeTypes: ["qr"],
+      }}
+    />
+  );
 }
